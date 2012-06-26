bd1296f2131ef1fb3c548c68855ce85b616b5737
Natapare <pare.kitty@hotmail.com>
2012-06-26 12:10:31 +0700

    ���� "Programming is fun"

--- src/th/ac/nu/students/u54347545/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347545/compro/ch01/HelloWorld.java
@@ -4,6 +4,6 @@ package th.ac.nu.students.u54347545.compro.ch01;
 public class HelloWorld {
     public static void main(String[]arg){
         System.out.println("Hello,World");
-        
+        System.out.println("Programming is fun");
     }
  }
