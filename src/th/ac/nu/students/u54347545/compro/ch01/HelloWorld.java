d14239689618b88aedef7615b189229d6d34b321
Natapare <pare.kitty@hotmail.com>
2012-06-26 12:09:11 +0700

    new HelloWorld

---
+++ src/th/ac/nu/students/u54347545/compro/ch01/HelloWorld.java
@@ -0,0 +1,9 @@
+
+package th.ac.nu.students.u54347545.compro.ch01;
+
+public class HelloWorld {
+    public static void main(String[]arg){
+        System.out.println("Hello,World");
+        
+    }
+ }
